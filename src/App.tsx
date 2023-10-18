b3f54eb4f4f89a7076c4521fb3a724b3e646bdc0
CoderCoco <christopher.jb31@gmail.com>
2023-10-17 20:52:49 -0500

    create qr code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import QRCodeSpiral from './qr-code/QrCodeSpiral';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QRCodeSpiral url='https://www.google.com'></QRCodeSpiral>
     </div>
   );
 }

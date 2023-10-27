323cba45a5dae73bcf24596275f60513d4c31165
CoderCoco <christopher.jb31@gmail.com>
2023-10-27 13:10:06 -0500

    add badge animation

--- src/cast-and-crew/BadgeList.tsx
+++ src/cast-and-crew/BadgeList.tsx
@@ -1,5 +1,5 @@
 import IDBadge from './IDBadge';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import "./BadgeList.css"
 
 interface IDBadgeProps {
@@ -19,54 +19,70 @@ interface IDBadgeProps {
 
 export default function BadgeList() {
   const [badges, setBadges] = useState<IDBadgeProps[]>([
-    {
-      name: 'MilesG170',
-      imageUrl: '/milesg170.png',
-      linkTree: 'https://www.google.com'
-    },
-    {
-      name: 'E To Interact',
-      imageUrl: '/missing-image.jpg',
-      linkTree: 'https://www.google.com'
-    },
-    {
-      name: 'Xenostream38',
-      imageUrl: '/xenostream38.png',
-      linkTree: 'https://www.google.com'
-    },
-    {
-      name: 'ToastGB',
-      imageUrl: '/missing-image.jpg',
-      linkTree: 'https://www.google.com'
-    },
+    
   ]);
-  const [insertIndex, setInsertIndex] = useState<number | null>(null);
+  const [insertName, setInsertName] = useState(new Set<string>());
 
-  const addBadge = (index: number) => {
-    const newBadge: IDBadgeProps = { name: 'New Person', imageUrl: '/missing-image.jpg', linkTree: 'https://www.google.com' };
-    const newBadges = [...badges];
-    newBadges.splice(index, 0, newBadge);
+  useEffect(() => {
+    const initialBadges: IDBadgeProps[] = [
+      {
+        name: 'MilesG170',
+        imageUrl: '/milesg170.png',
+        linkTree: 'https://linktr.ee/MilesG170'
+      },
+      {
+        name: 'E To Interact',
+        imageUrl: '/missing-image.jpg',
+        linkTree: 'https://linktr.ee/EToInteract'
+      },
+      {
+        name: 'Xenostream38',
+        imageUrl: '/xenostream38.png',
+        linkTree: 'https://linktr.ee/xenostream38'
+      },
+      {
+        name: 'ToastGB',
+        imageUrl: '/missing-image.jpg',
+        linkTree: 'https://linktr.ee/toastgb'
+      }
+    ];
+
+    initialBadges.forEach((badge, index) => {
+      setTimeout(() => {
+        setBadges(prevBadges => [...prevBadges, badge]);
+        setInsertName(prevNames => new Set([...prevNames, badge.name]));
+      }, index * 600); // 600ms delay between each badge
+    });
+  }, [])
+
+  const addBadge = (newPerson: IDBadgeProps) => {
+    const newBadges = [...badges, newPerson];
+    // Sort badges by name in alphabetical order
+    newBadges.sort((a, b) => a.name.localeCompare(b.name));
+
     setBadges(newBadges);
-    setInsertIndex(index);
+    setInsertName(new Set([newPerson.name]))
   };
+
+  const addCoderCoco = () => {
+    addBadge({name: "CoderCoco", linkTree: 'https://linktr.ee/codercoco', imageUrl: './CoinLogo_Resized.png'})
+  }
 
   // Reset animation state after it completes
   useEffect(() => {
-    if (insertIndex !== null) {
-      const timer = setTimeout(() => setInsertIndex(null), 500);
+    if (insertName.size > 0) {
+      const timer = setTimeout(() => setInsertName(new Set()), 500);
       return () => clearTimeout(timer);
     }
-  }, [insertIndex]);
+  }, [insertName]);
 
   return (
     <div>
-      <button onClick={() => addBadge(0)}>Add Badge at Start</button>
-      <button onClick={() => addBadge(Math.floor(badges.length / 2))}>Add Badge in Middle</button>
-      <button onClick={() => addBadge(badges.length)}>Add Badge at End</button>
+      <button onClick={() => addCoderCoco()}>Add CoderCoco</button>
       <div className="badge-list">
         {
-          badges.map((badge, index) => (
-            <div className={`badge-item ${index === insertIndex ? 'animate-insert' : ''}`}>
+          badges.map((badge) => (
+            <div className={`badge-item ${insertName.has(badge.name) ? 'animate-insert' : ''}`}>
               <IDBadge name={badge.name} imageUrl={badge.imageUrl} linkTree={badge.linkTree} />
             </div>
           ))
